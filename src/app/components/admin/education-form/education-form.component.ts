1eb9c1ae53db1274e877427d0ff505d19eda69c3
anisiqbal484 <anisiqbal484@gmail.com>
2024-12-17 22:32:54 +0500

    Education-Form done

--- src/app/components/admin/education-form/education-form.component.ts
+++ src/app/components/admin/education-form/education-form.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-education-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './education-form.component.html',
   styleUrl: './education-form.component.scss'
 })
 export class EducationFormComponent {
+
+  educationForm!: FormGroup;
+
+  constructor(){}
+
+  ngOnInit(){
+    this.educationForm= new FormGroup({
+      image: new FormControl(),
+      universityName: new FormControl(),
+      universityDegree : new FormControl(),
+      universityTenure: new FormControl(),
+    })
+  }
+
+  register(){
+    console.log(this.educationForm.value)
+  }
 
 }
